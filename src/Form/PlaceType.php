0c022e4fc8f011c3a66b11ef8317f50467e25c1f
Ev3ntyr <fanny.santamaria2023@campus-eni.fr>
2023-11-03 16:07:18 +0100

    Creating place type form

---
+++ src/Form/PlaceType.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\City;
+use App\Entity\Place;
+use Doctrine\DBAL\Types\TextType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PlaceType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'label' => 'Nom du lieu : '
+            ])
+            ->add('street', TextType::class, [
+                'label' => 'Rue : '
+            ])
+            ->add('latitude', NumberType::class, [
+                'label' => 'Latitude : '
+            ])
+            ->add('longitude', NumberType::class, [
+                'label' => 'Longitude : '
+            ])
+            ->add('city', EntityType::class, [
+                'label'=> 'Ville : ',
+                'class' => City::class,
+                'choice_label' => 'name',
+                'placeholder'=> '--Choisir une ville--'
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Place::class,
+        ]);
+    }
+}
